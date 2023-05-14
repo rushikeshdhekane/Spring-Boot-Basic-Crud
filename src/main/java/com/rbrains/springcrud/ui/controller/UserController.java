b77c51ba609b8e9f1d91ef496f2c745c9bdc7471
Rushikesh Dhekane <dhekanerushikesh@gmail.com>
2023-05-14 11:50:29 +0530

    model and controller changes

--- src/main/java/com/rbrains/springcrud/ui/controller/UserController.java
+++ src/main/java/com/rbrains/springcrud/ui/controller/UserController.java
@@ -1,20 +1,32 @@
 package com.rbrains.springcrud.ui.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rbrains.springcrud.ui.model.request.UserDetailRequestModel;
 import com.rbrains.springcrud.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users") //http://localhost:8080/users
 public class UserController {
+	
+	Map<String, UserRest> users;
 	
 	@GetMapping
 	public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
@@ -28,25 +40,62 @@ public class UserController {
 			    		MediaType.APPLICATION_XML_VALUE,
 			    		MediaType.APPLICATION_JSON_VALUE
 			    		})
-	public UserRest getUser(@PathVariable String userId) 
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId) 
 	{
-		UserRest userRest = new UserRest();
-		userRest.setEmail("r@gmail.com");
-		userRest.setFirstName("rushi");
-		userRest.setLastName("dhekane");
-		userRest.setUserId(userId);
+		if(users.containsKey(userId)) {
+			return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
+		}
+		else
+		{
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
 		
-		return userRest;
+		
 	}
 	
-	@PostMapping
-	public String createUser() {
-		return "create user get called";
+	@PostMapping(
+			consumes = {
+					MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_VALUE
+						},
+			produces = {
+					MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_VALUE
+			})
+	public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailRequestModel userDetailRequestModel) 
+	{
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail(userDetailRequestModel.getEmail());
+		returnValue.setFirstName(userDetailRequestModel.getFirstName());
+		returnValue.setLastName(userDetailRequestModel.getLastName());
+		
+		String userId = UUID.randomUUID().toString();
+		returnValue.setUserId(userId);
+		
+		if(users == null)users= new HashMap<>(); 
+		users.put(userId, returnValue);
+		
+		return new ResponseEntity<UserRest>(returnValue,HttpStatus.OK);
 	}
 	
-	@PutMapping
-	public String UpdateUser() {
-		return "update user get called";
+	@PutMapping(path = "/{userId}",
+			consumes = {
+					MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_VALUE
+						},
+			produces = {
+					MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_VALUE
+			})
+	public UserRest UpdateUser(@PathVariable String userId, @RequestBody UserDetailRequestModel model) {
+		
+		UserRest storedUserDetails = users.get(userId);
+		storedUserDetails.setFirstName(model.getFirstName());
+		storedUserDetails.setLastName(model.getLastName());
+		
+		users.put(userId, storedUserDetails);
+		
+		return storedUserDetails;
 	}
 	
 	@DeleteMapping
